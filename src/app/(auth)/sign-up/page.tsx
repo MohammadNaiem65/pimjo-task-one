b91dc4a2cfd204995d2dd72940dd6bcda66eac3e
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2025-11-13 19:19:41 +0600

    feat(auth/page): create /sign-in and /sign-up page
    Created sign in and sign up page

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -4,20 +4,20 @@ import type { Metadata } from "next";
 
 export function generateMetadata(): Metadata {
   return {
-    title: "Login | Pimjo",
-    description: "Login to your account | Pimjo",
+    title: "Register | Pimjo",
+    description: "Register to your account | Pimjo",
   };
 }
 
 export default function LoginPage() {
   return (
     <div>
-      <Header type="sign-in" />
+      <Header type="sign-up" />
 
       <div className="w-full border-y">
         <div className="max-content-width diagonal-background-pattern mx-auto h-full border-x">
           <div className="pattern-section-content-width flex h-full items-center justify-center border-x bg-white">
-            <AuthForm type="sign-in" />
+            <AuthForm type="sign-up" />
           </div>
         </div>
       </div>
